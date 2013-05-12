b61b6c440a7e0788748f38df526637f8f41094d5
lucas <lucas@ubuntu>
2013-05-11 22:59:17 -0300

    Update.

--- Server/Debug/src/main.d
+++ Server/Debug/src/main.d
@@ -6,7 +6,7 @@ src/main.d: ../src/main.c ../src/planificador.h \
  /home/lucas/workspace/commons/commons/config.h \
  /home/lucas/workspace/commons/commons/collections/dictionary.h \
  /home/lucas/workspace/commons/commons/string.h \
- /home/lucas/git/tp-20131c-tp-so-1c2013/uncommons/uncommons/fileStructures.h \
+ /home/lucas/workspace/uncommons/uncommons/fileStructures.h \
  /home/lucas/workspace/commons/commons/collections/list.h
 
 ../src/planificador.h:
@@ -25,6 +25,6 @@ src/main.d: ../src/main.c ../src/planificador.h \
 
 /home/lucas/workspace/commons/commons/string.h:
 
-/home/lucas/git/tp-20131c-tp-so-1c2013/uncommons/uncommons/fileStructures.h:
+/home/lucas/workspace/uncommons/uncommons/fileStructures.h:
 
 /home/lucas/workspace/commons/commons/collections/list.h:
--- Server/Debug/src/planificador.d
+++ Server/Debug/src/planificador.d
@@ -2,10 +2,10 @@ src/planificador.d: ../src/planificador.c \
  /home/lucas/workspace/commons/commons/collections/queue.h \
  /home/lucas/workspace/commons/commons/collections/list.h \
  /home/lucas/workspace/commons/commons/collections/node.h \
- /home/lucas/git/tp-20131c-tp-so-1c2013/uncommons/uncommons/SocketsBasic.h \
- /home/lucas/git/tp-20131c-tp-so-1c2013/uncommons/uncommons/SocketsServer.h \
+ /home/lucas/workspace/uncommons/uncommons/SocketsBasic.h \
+ /home/lucas/workspace/uncommons/uncommons/SocketsServer.h \
  ../src/planificador.h /home/lucas/workspace/commons/commons/string.h \
- /home/lucas/git/tp-20131c-tp-so-1c2013/uncommons/uncommons/fileStructures.h \
+ /home/lucas/workspace/uncommons/uncommons/fileStructures.h \
  /home/lucas/workspace/commons/commons/collections/list.h \
  /home/lucas/workspace/commons/commons/collections/dictionary.h
 
@@ -15,15 +15,15 @@ src/planificador.d: ../src/planificador.c \
 
 /home/lucas/workspace/commons/commons/collections/node.h:
 
-/home/lucas/git/tp-20131c-tp-so-1c2013/uncommons/uncommons/SocketsBasic.h:
+/home/lucas/workspace/uncommons/uncommons/SocketsBasic.h:
 
-/home/lucas/git/tp-20131c-tp-so-1c2013/uncommons/uncommons/SocketsServer.h:
+/home/lucas/workspace/uncommons/uncommons/SocketsServer.h:
 
 ../src/planificador.h:
 
 /home/lucas/workspace/commons/commons/string.h:
 
-/home/lucas/git/tp-20131c-tp-so-1c2013/uncommons/uncommons/fileStructures.h:
+/home/lucas/workspace/uncommons/uncommons/fileStructures.h:
 
 /home/lucas/workspace/commons/commons/collections/list.h:
 
