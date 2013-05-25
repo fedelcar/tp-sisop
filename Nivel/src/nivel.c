24c95239ee6c302647262e53a46edfa013882689
Luke90 <lucas.moreyra@gmail.com>
2013-05-24 21:03:51 -0300

    Update nivel.c
    Esta listo, pero no probado, la funcion "something" representa a la próxima función a crear para ejecutar para cada personaje que llega.
    Estructura en nivel.h - REVISAR
    
    Investigar para poner un pool de threads.

--- Nivel/src/nivel.c
+++ Nivel/src/nivel.c
@@ -13,10 +13,16 @@
 #include <pthread.h>
 #include <uncommons/SocketsServer.h>
 #include <uncommons/SocketsBasic.h>
+#include "nivel.h"
 #define MAXSIZE 1024
 #define DOSPUNTOS ":"
 
-void openListener(char* argv, queue_n_locks *queue );
+void openListener(char* argv, queue_n_locks *queue);
+
+void something(resource_struct *resourceStruct) {
+}
+resource_struct *getLevelStructure(t_level_config *level_config, int *fd,
+		pthread_mutex_t *resourcesReadLock, pthread_mutex_t *resourcesWriteLock);
 
 int main(char *argv) {
 
@@ -26,14 +32,16 @@ int main(char *argv) {
 
 	nivel = dictionary_get(niveles, argv);
 
-	pthread_mutex_t *readLock = (pthread_mutex_t*) malloc(sizeof(pthread_mutex_t));
-	pthread_mutex_t *writeLock = (pthread_mutex_t*) malloc(sizeof(pthread_mutex_t));
+	pthread_mutex_t *readLock = (pthread_mutex_t*) malloc(
+			sizeof(pthread_mutex_t));
+	pthread_mutex_t *writeLock = (pthread_mutex_t*) malloc(
+			sizeof(pthread_mutex_t));
 
 	queue_n_locks *queue = (queue_n_locks*) malloc(sizeof(queue_n_locks));
 
-	pthread_mutex_init(readLock, NULL);
+	pthread_mutex_init(readLock, NULL );
 
-	pthread_mutex_init(writeLock, NULL);
+	pthread_mutex_init(writeLock, NULL );
 
 	queue->character_queue = queue_create();
 	queue->readLock = readLock;
@@ -43,7 +51,16 @@ int main(char *argv) {
 
 	openListener(argv, queue);
 
-	while(1){
+	pthread_mutex_t *resourcesReadLock = (pthread_mutex_t*) malloc(
+			sizeof(pthread_mutex_t));
+	pthread_mutex_t *resourcesWriteLock = (pthread_mutex_t*) malloc(
+			sizeof(pthread_mutex_t));
+
+	pthread_mutex_init(resourcesReadLock, NULL );
+
+	pthread_mutex_init(resourcesWriteLock, NULL );
+
+	while (1) {
 
 		if (queue_size(queue->character_queue) == 0) {
 			pthread_mutex_lock(readLock);
@@ -55,19 +72,26 @@ int main(char *argv) {
 
 		fd = (int *) queue_pop(queue->character_queue);
 
+		resource_struct *resourceStruct = getLevelStructure(nivel, fd,
+				resourcesReadLock, resourcesWriteLock);
+
+		pthread_t *t = (pthread_t*) malloc(sizeof(pthread_t));
+
+		pthread_create(t, NULL, (void*) something,
+				(resource_struct*) resourceStruct);
 
 	}
 
 }
 
-
-void openListener(char* argv, queue_n_locks *queue){
+void openListener(char* argv, queue_n_locks *queue) {
 
 	t_dictionary *levelsMap = dictionary_create();
 
 	levelsMap = getLevelsMap();
 
-	t_level_address *addresses = (t_level_address *) malloc(sizeof(t_level_address));
+	t_level_address *addresses = (t_level_address *) malloc(
+			sizeof(t_level_address));
 
 	addresses = (t_level_address*) dictionary_get(levelsMap, argv);
 
@@ -77,9 +101,23 @@ void openListener(char* argv, queue_n_locks *queue){
 
 	pthread_t t;
 
-	pthread_create(&t, NULL, (int *) openSocketServer,(queue_n_locks *)queue);
+	pthread_create(&t, NULL, (int *) openSocketServer, (queue_n_locks *) queue);
 
 	free(port);
 	free(levelsMap);
 	free(addresses);
+}
+
+resource_struct *getLevelStructure(t_level_config *level_config, int *fd,
+		pthread_mutex_t *resourcesReadLock, pthread_mutex_t *resourcesWriteLock) {
+
+	resource_struct *resources = (resource_struct*) malloc(
+			sizeof(resource_struct));
+
+	resources->level_config = level_config;
+	resources->readLock = resourcesReadLock;
+	resources->writeLock = resourcesWriteLock;
+	resources->fd = fd;
+
+	return resources;
 }
