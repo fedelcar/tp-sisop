547ff79219b12c4101f079361bb8e695d0721298
fedelcar <fedelc@outlook.com>
2013-06-30 23:28:18 -0300

    Testeado Koopa

---
+++ Plataforma.sh
@@ -0,0 +1,18 @@
+cd
+cd git/tp-20131c-tp-so-1c2013/commons/Debug
+make clean
+make
+cd
+cd git/tp-20131c-tp-so-1c2013/uncommons/Debug
+make clean
+make
+cd
+cd git/tp-20131c-tp-so-1c2013/Server/Debug
+make clean
+make
+cd
+cd git/tp-20131c-tp-so-1c2013/Server/Debug
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/git/tp-20131c-tp-so-1c2013/uncommons/Debug
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/git/tp-20131c-tp-so-1c2013/commons/Debug
+ldd Server
+./Server
