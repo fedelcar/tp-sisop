c04c0c65ba20e48187948a6b96ca9a562bd8c8ed
fedelcar <fedelc@outlook.com>
2013-07-13 10:35:21 -0300

    Se agrega la carpeta debug

---
+++ personaje/Debug/src/personaje.d
@@ -0,0 +1,43 @@
+src/personaje.d: ../src/personaje.c \
+ /home/federico/git/tp-20131c-tp-so-1c2013/so-commons-library/commons/temporal.h \
+ /home/federico/git/tp-20131c-tp-so-1c2013/so-commons-library/commons/config.h \
+ /home/federico/git/tp-20131c-tp-so-1c2013/so-commons-library/commons/collections/dictionary.h \
+ /home/federico/git/tp-20131c-tp-so-1c2013/so-commons-library/commons/collections/node.h \
+ /home/federico/git/tp-20131c-tp-so-1c2013/so-commons-library/commons/log.h \
+ /home/federico/git/tp-20131c-tp-so-1c2013/so-commons-library/commons/string.h \
+ /home/federico/git/tp-20131c-tp-so-1c2013/so-commons-library/commons/collections/list.h \
+ /home/federico/git/tp-20131c-tp-so-1c2013/so-commons-library/commons/collections/node.h \
+ /home/federico/git/tp-20131c-tp-so-1c2013/uncommons/uncommons/fileStructures.h \
+ /home/federico/git/tp-20131c-tp-so-1c2013/so-commons-library/commons/collections/dictionary.h \
+ /home/federico/git/tp-20131c-tp-so-1c2013/so-commons-library/commons/collections/queue.h \
+ /home/federico/git/tp-20131c-tp-so-1c2013/so-commons-library/commons/collections/list.h \
+ /home/federico/git/tp-20131c-tp-so-1c2013/uncommons/uncommons/SocketsCliente.h \
+ /home/federico/git/tp-20131c-tp-so-1c2013/uncommons/uncommons/SocketsBasic.h
+
+/home/federico/git/tp-20131c-tp-so-1c2013/so-commons-library/commons/temporal.h:
+
+/home/federico/git/tp-20131c-tp-so-1c2013/so-commons-library/commons/config.h:
+
+/home/federico/git/tp-20131c-tp-so-1c2013/so-commons-library/commons/collections/dictionary.h:
+
+/home/federico/git/tp-20131c-tp-so-1c2013/so-commons-library/commons/collections/node.h:
+
+/home/federico/git/tp-20131c-tp-so-1c2013/so-commons-library/commons/log.h:
+
+/home/federico/git/tp-20131c-tp-so-1c2013/so-commons-library/commons/string.h:
+
+/home/federico/git/tp-20131c-tp-so-1c2013/so-commons-library/commons/collections/list.h:
+
+/home/federico/git/tp-20131c-tp-so-1c2013/so-commons-library/commons/collections/node.h:
+
+/home/federico/git/tp-20131c-tp-so-1c2013/uncommons/uncommons/fileStructures.h:
+
+/home/federico/git/tp-20131c-tp-so-1c2013/so-commons-library/commons/collections/dictionary.h:
+
+/home/federico/git/tp-20131c-tp-so-1c2013/so-commons-library/commons/collections/queue.h:
+
+/home/federico/git/tp-20131c-tp-so-1c2013/so-commons-library/commons/collections/list.h:
+
+/home/federico/git/tp-20131c-tp-so-1c2013/uncommons/uncommons/SocketsCliente.h:
+
+/home/federico/git/tp-20131c-tp-so-1c2013/uncommons/uncommons/SocketsBasic.h:
---
+++ uncommons/Debug/uncommons/SocketsServer.d
@@ -0,0 +1,13 @@
+uncommons/SocketsServer.d: ../uncommons/SocketsServer.c \
+ /home/federico/git/tp-20131c-tp-so-1c2013/so-commons-library/commons/collections/queue.h \
+ /home/federico/git/tp-20131c-tp-so-1c2013/so-commons-library/commons/collections/list.h \
+ /home/federico/git/tp-20131c-tp-so-1c2013/so-commons-library/commons/collections/node.h \
+ ../uncommons/SocketsBasic.h
+
+/home/federico/git/tp-20131c-tp-so-1c2013/so-commons-library/commons/collections/queue.h:
+
+/home/federico/git/tp-20131c-tp-so-1c2013/so-commons-library/commons/collections/list.h:
+
+/home/federico/git/tp-20131c-tp-so-1c2013/so-commons-library/commons/collections/node.h:
+
+../uncommons/SocketsBasic.h:
