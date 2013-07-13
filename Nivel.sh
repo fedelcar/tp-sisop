8393980f99c63a4fe479ec4a148f9858adeef359
PalumboN <nahuel.palumbo@gmail.com>
2013-07-13 09:29:42 -0300

    Update Nivel.sh

--- Nivel.sh
+++ Nivel.sh
@@ -1,23 +1,23 @@
 cd
-cd git/tp-20131c-tp-so-1c2013/Libreria-nivel/Debug
+cd /home/tp/git/tp-20131c-tp-so-1c2013/Libreria-nivel/Debug
 make clean
 make
 cd
-cd git/tp-20131c-tp-so-1c2013/so-commons-library/Debug
+cd /home/tp/git/tp-20131c-tp-so-1c2013/so-commons-library/Debug
 make clean
 make
 cd
-cd git/tp-20131c-tp-so-1c2013/uncommons/Debug
+cd /home/tp/git/tp-20131c-tp-so-1c2013/uncommons/Debug
 make clean
 make
 cd
-cd git/tp-20131c-tp-so-1c2013/Server/Debug
+cd /home/tp/git/tp-20131c-tp-so-1c2013/Server/Debug
 make clean
 make
 cd
-cd git/tp-20131c-tp-so-1c2013/Nivel/Debug
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/git/tp-20131c-tp-so-1c2013/uncommons/Debug
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/git/tp-20131c-tp-so-1c2013/commons/Debug
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/git/tp-20131c-tp-so-1c2013/Libreria-nivel/Debug
+cd /home/tp/git/tp-20131c-tp-so-1c2013/Nivel/Debug
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/tp/git/tp-20131c-tp-so-1c2013/uncommons/Debug
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/tp/git/tp-20131c-tp-so-1c2013/so-commons-library/Debug
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/tp/git/tp-20131c-tp-so-1c2013/Libreria-nivel/Debug
 ldd Nivel
-./Nivel
+./Nivel $1
