0ef7b7f1c156888d5a7fa1682ec70722fe11ef2f
fedelcar <fedelc@outlook.com>
2013-07-25 20:21:36 -0300

    Arreglados y agregados archivos sh

--- Personaje.sh
+++ Personaje.sh
@@ -1,16 +1,3 @@
-cd
-cd /home/utnso/git/tp-20131c-tp-so-1c2013/so-commons-library/Debug
-make clean
-make
-cd
-cd /home/utnso/git/tp-20131c-tp-so-1c2013/uncommons/Debug
-make clean
-make
-cd
-cd /home/utnso/git/tp-20131c-tp-so-1c2013/personaje/Debug
-make clean
-make
-cd
 cd /home/utnso/git/tp-20131c-tp-so-1c2013/personaje/Debug
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/utnso/git/tp-20131c-tp-so-1c2013/uncommons/Debug
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/utnso/git/tp-20131c-tp-so-1c2013/so-commons-library/Debug
