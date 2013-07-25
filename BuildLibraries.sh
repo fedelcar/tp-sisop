fcca3dd7ac2d3ddd9480ace172c091b4ee642933
fedelcar <fedelc@outlook.com>
2013-07-25 20:20:55 -0300

    Arreglados y agregados archivos sh

---
+++ BuildLibraries.sh
@@ -0,0 +1,13 @@
+cd /home/utnso/git/tp-20131c-tp-so-1c2013/Libreria-nivel/Debug
+make clean
+make
+cd /home/utnso/git/tp-20131c-tp-so-1c2013/so-commons-library/Debug
+make clean
+make
+cd /home/utnso/git/tp-20131c-tp-so-1c2013/uncommons/Debug
+make clean
+make
+cd /home/utnso/git/tp-20131c-tp-so-1c2013/Server/Debug
+make clean
+make
+
---
+++ Start.sh
@@ -0,0 +1,11 @@
+cd /home
+mkdir tp
+cd tp
+mkdir config
+cd config
+mkdir logs
+cd /home/utnso
+mkdir git
+cd git
+git clone https://github.com/sisoputnfrba/tp-20131c-tp-so-1c2013.git
+
